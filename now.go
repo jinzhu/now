f895bc589457306c26f3a56b7c39db364d2c3075
Jinzhu <wosmvp@gmail.com>
2018-03-14 09:06:32 +0800

    Add comments

--- now.go
+++ now.go
@@ -6,19 +6,23 @@ import (
 	"time"
 )
 
+// BeginningOfMinute beginning of minute
 func (now *Now) BeginningOfMinute() time.Time {
 	return now.Truncate(time.Minute)
 }
 
+// BeginningOfHour beginning of hour
 func (now *Now) BeginningOfHour() time.Time {
 	return now.Truncate(time.Hour)
 }
 
+// BeginningOfDay beginning of day
 func (now *Now) BeginningOfDay() time.Time {
 	d := time.Duration(-now.Hour()) * time.Hour
 	return now.BeginningOfHour().Add(d)
 }
 
+// BeginningOfWeek beginning of week
 func (now *Now) BeginningOfWeek() time.Time {
 	t := now.BeginningOfDay()
 	weekday := int(t.Weekday())
@@ -37,52 +41,63 @@ func (now *Now) BeginningOfWeek() time.Time {
 	return t.Add(d)
 }
 
+// BeginningOfMonth beginning of month
 func (now *Now) BeginningOfMonth() time.Time {
 	t := now.BeginningOfDay()
 	d := time.Duration(-int(t.Day())+1) * 24 * time.Hour
 	return t.Add(d)
 }
 
+// BeginningOfQuarter beginning of quarter
 func (now *Now) BeginningOfQuarter() time.Time {
 	month := now.BeginningOfMonth()
 	offset := (int(month.Month()) - 1) % 3
 	return month.AddDate(0, -offset, 0)
 }
 
+// BeginningOfYear BeginningOfYear beginning of year
 func (now *Now) BeginningOfYear() time.Time {
 	t := now.BeginningOfDay()
 	d := time.Duration(-int(t.YearDay())+1) * 24 * time.Hour
 	return t.Truncate(time.Hour).Add(d)
 }
 
+// EndOfMinute end of minute
 func (now *Now) EndOfMinute() time.Time {
 	return now.BeginningOfMinute().Add(time.Minute - time.Nanosecond)
 }
 
+// EndOfHour end of hour
 func (now *Now) EndOfHour() time.Time {
 	return now.BeginningOfHour().Add(time.Hour - time.Nanosecond)
 }
 
+// EndOfDay end of day
 func (now *Now) EndOfDay() time.Time {
 	return now.BeginningOfDay().Add(24*time.Hour - time.Nanosecond)
 }
 
+// EndOfWeek end of week
 func (now *Now) EndOfWeek() time.Time {
 	return now.BeginningOfWeek().AddDate(0, 0, 7).Add(-time.Nanosecond)
 }
 
+// EndOfMonth end of month
 func (now *Now) EndOfMonth() time.Time {
 	return now.BeginningOfMonth().AddDate(0, 1, 0).Add(-time.Nanosecond)
 }
 
+// EndOfQuarter end of quarter
 func (now *Now) EndOfQuarter() time.Time {
 	return now.BeginningOfQuarter().AddDate(0, 3, 0).Add(-time.Nanosecond)
 }
 
+// EndOfYear end of year
 func (now *Now) EndOfYear() time.Time {
 	return now.BeginningOfYear().AddDate(1, 0, 0).Add(-time.Nanosecond)
 }
 
+// Monday monday
 func (now *Now) Monday() time.Time {
 	t := now.BeginningOfDay()
 	weekday := int(t.Weekday())
@@ -93,17 +108,19 @@ func (now *Now) Monday() time.Time {
 	return t.Truncate(time.Hour).Add(d)
 }
 
+// Sunday sunday
 func (now *Now) Sunday() time.Time {
 	t := now.BeginningOfDay()
 	weekday := int(t.Weekday())
 	if weekday == 0 {
 		return t
-	} else {
-		d := time.Duration(7-weekday) * 24 * time.Hour
-		return t.Truncate(time.Hour).Add(d)
 	}
+
+	d := time.Duration(7-weekday) * 24 * time.Hour
+	return t.Truncate(time.Hour).Add(d)
 }
 
+// EndOfSunday end of sunday
 func (now *Now) EndOfSunday() time.Time {
 	return now.Sunday().Add(24*time.Hour - time.Nanosecond)
 }
@@ -119,6 +136,7 @@ func parseWithFormat(str string) (t time.Time, err error) {
 	return
 }
 
+// Parse parse string to time
 func (now *Now) Parse(strs ...string) (t time.Time, err error) {
 	var setCurrentTime bool
 	parseTime := []int{}
@@ -171,6 +189,7 @@ func (now *Now) Parse(strs ...string) (t time.Time, err error) {
 	return
 }
 
+// MustParse must parse string to time or it will panic
 func (now *Now) MustParse(strs ...string) (t time.Time) {
 	t, err := now.Parse(strs...)
 	if err != nil {
@@ -179,8 +198,9 @@ func (now *Now) MustParse(strs ...string) (t time.Time) {
 	return t
 }
 
-func (now *Now) Between(time1, time2 string) bool {
-	restime := now.MustParse(time1)
-	restime2 := now.MustParse(time2)
-	return now.After(restime) && now.Before(restime2)
+// Between check time between the begin, end time or not
+func (now *Now) Between(begin, end string) bool {
+	beginTime := now.MustParse(begin)
+	endTime := now.MustParse(end)
+	return now.After(beginTime) && now.Before(endTime)
 }
